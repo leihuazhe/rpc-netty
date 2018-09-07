206a080f45ec99f789cba1486b58b27230651670
leihuazhe <295482300@qq.com>
2018-09-07 16:01:36 +0800

    unpack 粘包拆包问题解决

---
+++ un-pack/src/main/java/com/maple/heartbeat/client/handler/RpcMsgDecoder.java
@@ -0,0 +1,56 @@
+package com.maple.heartbeat.client.handler;
+
+import com.google.gson.Gson;
+import com.maple.heartbeat.entity.RpcObject;
+import com.maple.rpc.common.util.RpcException;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static com.maple.heartbeat.util.FrameProtocolUtil.ETX;
+import static com.maple.heartbeat.util.FrameProtocolUtil.STX;
+import static io.netty.channel.ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE;
+
+
+/**
+ * @author maple 2018.09.07 上午9:40
+ */
+public class RpcMsgDecoder extends MessageToMessageDecoder<ByteBuf> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcMsgDecoder.class);
+
+    private Gson gson = new Gson();
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+        try {
+            if (LOGGER.isTraceEnabled()) {
+                LOGGER.trace(getClass().getSimpleName() + "::decode");
+            }
+            int readable = msg.readableBytes();
+
+            byte stx = msg.readByte();
+            // 通讯协议不正确
+            if (stx != STX) {
+                throw new RpcException("Err-Rpc-001", "通讯协议不正确(起始符)");
+            }
+
+            byte etx = msg.getByte(readable - 1);
+            // 通讯协议不正确
+            if (etx != ETX) {
+                throw new RpcException("Err-Rpc-002", "通讯协议不正确(结束符)");
+            }
+            String result = msg.readBytes(readable - 1).toString();
+            out.add(gson.fromJson(result, RpcObject.class));
+        } catch (RpcException e) {
+            LOGGER.error(e.getMessage(), e);
+            RpcObject rpcObject = new RpcObject(9999, e.getMessage());
+
+            ctx.writeAndFlush(rpcObject).addListener(FIRE_EXCEPTION_ON_FAILURE);
+        }
+    }
+
+}
---
+++ un-pack/src/main/java/com/maple/heartbeat/client/handler/RpcMsgEncoder.java
@@ -0,0 +1,45 @@
+package com.maple.heartbeat.client.handler;
+
+import com.google.gson.Gson;
+import com.maple.heartbeat.entity.RpcObject;
+import com.maple.rpc.common.util.RpcException;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static com.maple.heartbeat.util.FrameProtocolUtil.ETX;
+import static com.maple.heartbeat.util.FrameProtocolUtil.STX;
+import static io.netty.channel.ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE;
+
+
+/**
+ * @author maple 2018.09.07 上午9:40
+ */
+public class RpcMsgEncoder extends MessageToByteEncoder<RpcObject> {
+//    private static final Logger LOGGER = LoggerFactory.getLogger(RpcMsgEncoder.class);
+
+    private Gson gson = new Gson();
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, RpcObject msg, ByteBuf out) throws Exception {
+        int beginIndex = out.readerIndex();
+        String content = gson.toJson(msg);
+
+        //先写,占位
+        out.writeInt(0);
+        out.writeByte(STX);
+        out.writeBytes(content.getBytes(CharsetUtil.UTF_8));
+        out.writeByte(ETX);
+
+        int endIndex = out.writerIndex();
+        int length = endIndex - beginIndex - Integer.BYTES;
+        out.writerIndex(beginIndex).writeInt(length);
+        out.writerIndex(endIndex);
+    }
+}
